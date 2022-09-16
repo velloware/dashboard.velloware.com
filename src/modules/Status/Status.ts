501664d1159131b885fce2133c594e8d76ebf820
Colgate13 <gabreilbarros13@gmail.com>
2022-09-16 16:08:43 -0300

    Update logger

--- src/modules/Status/Status.ts
+++ src/modules/Status/Status.ts
@@ -3,29 +3,51 @@ import { Application } from './Application/Domain/Application';
 import { SendNotification } from '../Notification/useNotification';
 import cron from 'node-cron';
 
-cron.schedule('* * * * *', async () => {
-  console.log('running a task every minute');
+let LogErrorSendeds: Number[] = [];
 
+cron.schedule('* * * * *', async () => {
   getApplications().map((app: Application) => {
-    if (app.status !== 'online') {
-      SendNotification.send(`❗ Application ${app.name} is ${app.status}
+    if (app.status !== 'online' && !LogErrorSendeds.includes(app.pid)) {
+      SendNotification.send(`
+❗Application {${app.name}} is ${app.status}❗
+
 Server: ${app.serverName}
 Uptime: ${app.uptime}
 Restart Count: ${app.restartCount}
 CPU: ${app.cpu}
 Memory: ${app.memory}
 Mode: ${app.mode}`);
+
+      LogErrorSendeds.push(app.pid);
+    } else if (app.status === 'online' && LogErrorSendeds.includes(app.pid)) {
+      SendNotification.send(`
+💹Application {${app.name}} is ${app.status}💹
+      
+Server: ${app.serverName}
+Uptime: ${app.uptime}
+Restart Count: ${app.restartCount}
+CPU: ${app.cpu}
+Memory: ${app.memory}
+Mode: ${app.mode}`);
+
+      LogErrorSendeds = LogErrorSendeds.filter((pid: Number) => pid !== app.pid);
+
     }
+
+
   });
 
 });
 
+cron.schedule('0 0 */1 * * *', async () => {
+  LogErrorSendeds.length = 0;
+});
+
 
 cron.schedule('0 0 */6 * * *', async () => {
-  console.log('running a task every minute');
-
   getApplications().map((app: Application) => {
     SendNotification.send(`Application Report - ${app.name} is ${app.status}
+
 Server: ${app.serverName}
 Uptime: ${app.uptime}
 Restart Count: ${app.restartCount}
