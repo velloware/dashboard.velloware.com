2647dc72cd2c111c2f9596085aa375ca55b7beb7
Gabriel Barros Feitosa Sá <54006467+Colgate13@users.noreply.github.com>
2022-12-05 13:34:19 +0000

    Create format

--- dist/server.js
+++ dist/server.js
@@ -5,10 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const server_1 = __importDefault(require("./infra/http/server"));
+const Status_1 = require("./modules/Status/Status");
 dotenv_1.default.config();
 process.title = 'Velloware Dash - server';
 const serverHttp = new server_1.default(process.env.PORT || '5000');
 serverHttp.init();
+(0, Status_1.checkStatus)();
 process.on('SIGTERM', () => {
     console.log('> Server ending after close all connections - ', new Date().toISOString());
     serverHttp.close(() => process.exit());
